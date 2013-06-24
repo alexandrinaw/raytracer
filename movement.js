bdc0243c7fae65568c208312d71b876873a1a6c2
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-24 17:43:59 -0400

    Rework movement.js to call separated move fns on keydown.

--- movement.js
+++ movement.js
@@ -1,19 +1,44 @@
 ;(function(exports) {
-    var allowMovement = function () {
-        window.addEventListener("keydown", function(e) {
-            if(e.keyCode == 38) {
-                screen.moveDown();
-            }
-            if(e.keyCode == 40) {
-                screen.moveUp();
-            }
-            if(e.keyCode == 39) {
-                screen.moveLeft();
-            }
-            if(e.keyCode == 37) {
-                screen.moveRight();
-            }
-        })
+    var moves = {
+        moveRight : function (mover) {
+            mover.x = mover.x-50;
+        },
+
+        moveLeft : function (mover) {
+            mover.x = mover.x+50;
+        },
+
+        moveUp : function (mover) {
+            mover.y = mover.y+50;
+        },
+
+        moveDown : function (mover) {
+            mover.y = mover.y-50;
+        },
+
+        moveForward : function (mover) {
+            mover.z = mover.z-50;
+        },
+
+        moveBackward : function (mover) {
+            mover.z = mover.z+50;
+        }
     };
-    exports.allowMovement=allowMovement;
+
+    var moveMappings = [];
+    moveMappings[38] = moves.moveDown;
+    moveMappings[40] = moves.moveUp;
+    moveMappings[39] = moves.moveLeft;
+    moveMappings[37] = moves.moveRight;
+
+    var makeMoveable = function (mover, movedCallback) {
+        window.addEventListener("keydown", function(e) {
+            if (e.keyCode in moveMappings) {
+                moveMappings[e.keyCode](mover);
+                movedCallback();
+            }
+        });
+    };
+
+    exports.makeMoveable = makeMoveable;
 })(this);
