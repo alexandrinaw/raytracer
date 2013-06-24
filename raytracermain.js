3c6526ddeb3b981e47d59762e237f3ddb8b0e0fc
Thomas Ballinger <thomasballinger@gmail.com>
2013-06-24 14:36:49 -0400

    make objects in setUpScene local and formatting
    we prefer spaces before and after equal signs

--- raytracermain.js
+++ raytracermain.js
@@ -5,7 +5,7 @@ var screenDepth;
 var screen;  
 (function (exports) {
     function setUpScene() {
-        var p1=new Plane([400,0,0],[-1, 0, 0]);
+        var p1 = new Plane([400,0,0],[-1, 0, 0]);
         objects.push(p1); 
 
         var s = new Sphere(30, 75, -30, 40);
@@ -13,20 +13,20 @@ var screen;
         s.Material.lighting(0.6, 0.2, 0.2); 
         objects.push(s); 
 
-        var s2 = new Sphere (60, 150, -60, 20); 
+        var s2 = new Sphere(60, 150, -60, 20); 
         s2.Material.setColor(0, 77, 100); 
         s2.Material.lighting(0.3, 0.6, 0.1); 
         objects.push(s2);
 
-        s4=new Sphere(300, 250, -1000, 300); 
+        var s4 = new Sphere(300, 250, -1000, 300); 
         s4.Material.lighting(0.6, 0.3, 0.1); 
         objects.push(s4); 
 
-        var s3 = new Sphere (200,200,-200,40); 
+        var s3 = new Sphere(200,200,-200,40); 
         objects.push(s3); 
 
         var p = new Plane([100,300,100],[0,-1,0]);
-        p.Material=new Checkerboard(100); 
+        p.Material = new Checkerboard(100); 
         objects.push(p); 
 
         var l = new Light(250, 100, 100, 100);
