861dccfff808d302e7d401908d61b0777f20383d
Thomas Ballinger <thomasballinger@gmail.com>
2013-06-24 14:34:55 -0400

    organize initiation of scene

--- raytracermain.js
+++ raytracermain.js
@@ -7,24 +7,31 @@ var screen;
     function setUpScene() {
         var p1=new Plane([400,0,0],[-1, 0, 0]);
         objects.push(p1); 
+
         var s = new Sphere(30, 75, -30, 40);
         s.Material.setColor(30, 198, 0); 
         s.Material.lighting(0.6, 0.2, 0.2); 
+        objects.push(s); 
+
         var s2 = new Sphere (60, 150, -60, 20); 
         s2.Material.setColor(0, 77, 100); 
         s2.Material.lighting(0.3, 0.6, 0.1); 
+        objects.push(s2);
+
         s4=new Sphere(300, 250, -1000, 300); 
         s4.Material.lighting(0.6, 0.3, 0.1); 
-        objects.push(s); 
-        objects.push(s2);
         objects.push(s4); 
+
         var s3 = new Sphere (200,200,-200,40); 
         objects.push(s3); 
+
         var p = new Plane([100,300,100],[0,-1,0]);
         p.Material=new Checkerboard(100); 
         objects.push(p); 
+
         var l = new Light(250, 100, 100, 100);
         lights.push(l); 
+
         cam = new Camera(width/2, height/2, 500); 
         screen = new Screen();
     }
