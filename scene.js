cb751501955964abc427081cff3ea0cf42c5ead4
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-25 01:18:13 -0400

    Move closest_object() to Scene proto.

--- scene.js
+++ scene.js
@@ -45,7 +45,7 @@ var screenDepth;
                     var color=[0, 0, 0, 255];
                     var r = new ray(w+screenX, h+screenY, screenDepth, cam_x, cam_y, cam_z);
                     // var r = new ray(cam_x,cam_y, cam_z, w+screenX, h+screenY, screenDepth);
-                    var cl = closest_object(r);
+                    var cl = this.closest_object(r);
                     if (cl!==undefined) {
                         var lighting = lightBuilder(cl, r);
                         color[0]+=lighting[0];
@@ -56,6 +56,24 @@ var screenDepth;
                 }
             }
             return renderedImage;
+        },
+
+        closest_object: function(ry) {
+            var closest_dist=10000000;
+            var closest_i=-1;
+            for (var i=0; i<scene.objects.length; i++) {
+                var p = scene.objects[i].intersections(ry);
+                for (var j=0; j<p.length; j++) {
+                    var dist = Math.sqrt(Math.pow(ry.x-p[j][0], 2)+Math.pow(ry.y-p[j][1], 2)+Math.pow(ry.z-p[j][2], 2));
+                    if (dist<closest_dist) {
+                        closest_dist=dist;
+                        closest_i=i;
+                    }
+                }
+            }
+            if (closest_i>-1) {
+                return scene.objects[closest_i];
+            }
         }
     };
 
