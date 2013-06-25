9894156c708a39718ae3091254566d4d62a56602
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-24 23:55:46 -0400

    Put randomDots on Renderer.

--- drawpixels.js
+++ drawpixels.js
@@ -7,6 +7,19 @@
         this.imageData = this.c.createImageData(this.width, this.height);
     };
 
+    Renderer.prototype = {
+        randomDots: function() {
+            // draw random dots
+            for (i = 0; i < this.width*this.height; i++) {
+                x = Math.random() * this.width | 0; // |0 to truncate to Int32
+                y = Math.random() * this.height | 0;
+                r = Math.random() * 256 | 0;
+                g = Math.random() * 256 | 0;
+                b = Math.random() * 256 | 0;
+                this.setPixel(this.imageData, x, y, r, g, b, 255); // 255 opaque
+            }
+            this.c.putImageData(this.imageData, 0, 0);
+        },
 
 function setPixel (imageData, x, y, r, g, b, a) {
      index = (x + y * imageData.width) * 4;
@@ -16,17 +29,5 @@ function setPixel (imageData, x, y, r, g, b, a) {
      imageData.data[index+3] = a;
 }
 
-function randomDots (){
-    // draw random dots
-    for (i = 0; i < width*height; i++) {
-        x = Math.random() * width | 0; // |0 to truncate to Int32
-        y = Math.random() * height | 0;
-        r = Math.random() * 256 | 0;
-        g = Math.random() * 256 | 0;
-        b = Math.random() * 256 | 0;
-        setPixel(imageData, x, y, r, g, b, 255); // 255 opaque
-    }
-    c.putImageData(imageData, 0, 0);
-}
     exports.Renderer = Renderer;
 })(this);
